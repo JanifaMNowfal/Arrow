fe4589f4e91fedd27f5f3269ffb1ff7a6cc0e60e
JanifaMNowfal <janifanowfal@gmail.com>
2018-06-25 18:01:06 +0530

    First commit

---
+++ forwarder/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("hello there")
+
+def staff(request):
+    return render(request, 'forwarder/staff.html')
+
+def apply(request):
+    return render(request, 'forwarder/application_form.html')
+
+def detail(request):
+    return render(request, 'forwarder/detail.html')
